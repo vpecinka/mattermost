5689d0e673125496e7f3dcee1ede22b496d78676
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-10-30 14:55:42 +0100

    update available locales

--- server/public/shared/i18n/i18n.go
+++ server/public/shared/i18n/i18n.go
@@ -93,6 +93,7 @@ var supportedLocales = []string{
 	"zh-CN",
 	"zh-TW",
 	"ja",
+	"cs",
 }
 
 var (
--- webapp/channels/src/i18n/i18n.ts
+++ webapp/channels/src/i18n/i18n.ts
@@ -152,6 +152,12 @@ export const languages = {
         order: 21,
         url: langFiles.ja,
     },
+    cs: {
+        value: 'cs',
+        name: 'Čeština',
+        order: 22,
+        url: langFiles.cs,
+    },
 };
 
 export function getAllLanguages(includeExperimental = false): Record<string, Language> {
