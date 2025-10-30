ad84328866bfd838194aef2bb10d4c028353c978
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-10-30 11:34:58 +0100

    tune plugins

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
 
 export BUILD_NUMBER=szn-v11.0.2 
+
+# Plugins to pre-package
+PLUGIN_PACKAGES="mattermost-plugin-jira-v4.3.0"
+PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-gitlab-v1.10.0"
+PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-zoom-v1.8.0"
+PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-boards-v9.1.6"
+export PLUGIN_PACKAGES
+
+echo "Building BUILD_NUMBER=${BUILD_NUMBER}"
+echo "Plugins: ${PLUGIN_PACKAGES}"
+echo "Any key to continue, CTRL+C to break..."
+read
 
 
 # NOTE: How to update repo
@@ -19,6 +31,7 @@ export BUILD_NUMBER=szn-v11.0.2
 
 echo "\n===== BUILDING WEBAPP ================\n"
 cd webapp
+. ~/.nvm/nvm.sh
 make dist
 
 echo "\n===== BUILDING SERVER ================\n"
@@ -32,4 +45,5 @@ make package-linux-amd64
 cd ..
 
 echo "\n===== TESTING APP PACKAGE ============\n"
-tar tzf server/dist/mattermost-team-linux-amd64.tar.gz
+echo "Testing package server/dist/mattermost-team-linux-amd64.tar.gz"
+tar tzf server/dist/mattermost-team-linux-amd64.tar.gz >/dev/null && echo "Package build sucessfuly"
