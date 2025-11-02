1ed677047917f77e3aad4a4479c833139afe4b00
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-11-02 16:16:51 +0100

    limit message queue size

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -3090,6 +3090,7 @@ type SznSearchSettings struct {
 	IgnoreChannels        *string
 	IgnoreTeams           *string
 	ReindexChannelPool    *int
+	MessageQueueSize      *int
 }
 
 func (s *SznSearchSettings) SetDefaults() {
@@ -3155,6 +3156,10 @@ func (s *SznSearchSettings) SetDefaults() {
 
 	if s.ReindexChannelPool == nil {
 		s.ReindexChannelPool = NewPointer(20)
+	}
+
+	if s.MessageQueueSize == nil {
+		s.MessageQueueSize = NewPointer(10000)
 	}
 }
 
