271340e8471af16096c55a83d2cdc8194a3b3558
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-12-01 13:11:29 +0100

    update jira plugin

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ NC='\033[0m' # No Color
 export BUILD_NUMBER="szn-$(date +%y%m%d-%H%M)"
 
 # Plugins to pre-package
-PLUGIN_PACKAGES="mattermost-plugin-jira-v4.4.0"
+PLUGIN_PACKAGES="mattermost-plugin-jira-v4.4.1"
 PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-gitlab-v1.11.0"
 PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-zoom-v1.8.0"
 PLUGIN_PACKAGES="$PLUGIN_PACKAGES mattermost-plugin-boards-v9.1.7"
