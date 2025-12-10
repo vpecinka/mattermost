5856e34a3ef8862645509886d760d5fa72364c1a
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-12-10 12:27:17 +0100

    add copyright notices

--- server/custom/sznsearch/common/file_extractor.go
+++ server/custom/sznsearch/common/file_extractor.go
@@ -1,5 +1,17 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
+// Copyright (c) 2024-present Seznam.cz, a.s.
+// All Rights Reserved.
+//
+// This file is part of custom extensions to Mattermost server
+// developed by Seznam.cz, a.s. for internal use only.
+//
+// This code is a derivative work based on Mattermost server
+// (https://github.com/mattermost/mattermost-server)
+// Original Mattermost code is licensed under AGPL v3.0.
+//
+// This derivative work is used exclusively for internal purposes
+// within Seznam.cz, a.s. and is not distributed to third parties.
+// Therefore, the copyleft provisions of AGPL v3.0 do not apply
+// as per the license's distribution requirements.
 
 package common
 
