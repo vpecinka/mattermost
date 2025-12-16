021b7ea747974d78f55f481d421db16049fbcb3b
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-12-16 12:17:50 +0100

    remove cluster license check

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -1626,7 +1626,8 @@ const AdminDefinition: AdminDefinitionType = {
                 url: 'environment/high_availability',
                 title: defineMessage({id: 'admin.sidebar.highAvailability', defaultMessage: 'High Availability'}),
                 isHidden: it.any(
-                    it.not(it.licensedForFeature('Cluster')),
+                    // NOTE: Custom cluster implementation (szncluster) doesn't require enterprise license
+                    // it.not(it.licensedForFeature('Cluster')),
                     it.configIsTrue('ExperimentalSettings', 'RestrictSystemAdmin'),
                     it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
                 ),
@@ -1641,7 +1642,8 @@ const AdminDefinition: AdminDefinitionType = {
                 url: 'environment/cache_settings',
                 title: adminDefinitionMessages.cache_settings_title,
                 isHidden: it.any(
-                    it.not(it.licensedForFeature('Cluster')),
+                    // NOTE: Custom cluster implementation (szncluster) doesn't require enterprise license
+                    // it.not(it.licensedForFeature('Cluster')),
                     it.configIsTrue('ExperimentalSettings', 'RestrictSystemAdmin'),
                     it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
                 ),
