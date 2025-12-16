dd4a2fffe4994b8e9522dac2222fe3c68fc1487f
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2025-12-16 12:27:14 +0100

    show HA admin page

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -1629,7 +1629,7 @@ const AdminDefinition: AdminDefinitionType = {
                     // NOTE: Custom cluster implementation (szncluster) doesn't require enterprise license
                     // it.not(it.licensedForFeature('Cluster')),
                     it.configIsTrue('ExperimentalSettings', 'RestrictSystemAdmin'),
-                    it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
+                    // it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
                 ),
                 searchableStrings: clusterSearchableStrings,
                 isDisabled: it.not(it.userHasWritePermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
@@ -1645,7 +1645,7 @@ const AdminDefinition: AdminDefinitionType = {
                     // NOTE: Custom cluster implementation (szncluster) doesn't require enterprise license
                     // it.not(it.licensedForFeature('Cluster')),
                     it.configIsTrue('ExperimentalSettings', 'RestrictSystemAdmin'),
-                    it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
+                    // it.not(it.userHasReadPermissionOnResource(RESOURCE_KEYS.ENVIRONMENT.HIGH_AVAILABILITY)),
                 ),
                 searchableStrings: [
                     adminDefinitionMessages.cache_settings_title,
